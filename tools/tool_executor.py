d2e201b623e86b6dab36bf65467bfedd092bb400
Divyansh <divyansh1982006@gmail.com>
2026-08-15 01:19:37 +0530

    tool_executer is modified now

--- tools/tool_executor.py
+++ tools/tool_executor.py
@@ -3,17 +3,25 @@ from tools.abuseipdb import check_ip as abuseipdb_check
 from tools.virustotal import check_ip as virustotal_check
 
 
+TOOL_HANDLERS = {
+    "AbuseIPDB": abuseipdb_check,
+    "VirusTotal": virustotal_check,
+}
+
+
 def execute_tools(investigation: Investigation) -> Investigation:
     results = {}
 
     for tool in investigation.investigation_plan:
 
-        if tool == "AbuseIPDB" and investigation.ioc_type == "IP":
-            results[tool] = abuseipdb_check(investigation.ioc)
-
-        elif tool == "VirusTotal" and investigation.ioc_type == "IP":
-            results[tool] = virustotal_check(investigation.ioc)
-
+        if tool in TOOL_HANDLERS:
+            if investigation.ioc_type == "IP":
+                results[tool] = TOOL_HANDLERS[tool](investigation.ioc)
+            else:
+                results[tool] = {
+                    "status": "unsupported",
+                    "message": f"{tool} does not currently support {investigation.ioc_type}"
+                }
         else:
             results[tool] = {
                 "status": "pending",
