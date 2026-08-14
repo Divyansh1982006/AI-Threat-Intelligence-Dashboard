961fc9e33e686d840acc3fc3e681ca20371abb64
Divyansh <divyansh1982006@gmail.com>
2026-08-15 01:14:34 +0530

    investigation_agent.py is modified

--- agent/investigation_agent.py
+++ agent/investigation_agent.py
@@ -2,19 +2,47 @@ from models.investigation_model import Investigation
 
 
 TOOL_MAP = {
-    "IP": ["AbuseIPDB", "VirusTotal", "OTX", "GeoIP", "Shodan"],
-    "DOMAIN": ["VirusTotal", "WHOIS", "DNS", "OTX"],
-    "URL": ["VirusTotal", "URLScan", "OTX"],
-    "MD5": ["VirusTotal", "MalwareBazaar"],
-    "SHA1": ["VirusTotal", "MalwareBazaar"],
-    "SHA256": ["VirusTotal", "MalwareBazaar"],
-    "EMAIL": ["VirusTotal", "WHOIS"]
+    "IP": [
+        "AbuseIPDB",
+        "VirusTotal",
+        "OTX",
+        "GeoIP",
+        "Shodan"
+    ],
+    "DOMAIN": [
+        "VirusTotal",
+        "WHOIS",
+        "DNS",
+        "OTX"
+    ],
+    "URL": [
+        "VirusTotal",
+        "URLScan",
+        "OTX"
+    ],
+    "MD5": [
+        "VirusTotal",
+        "MalwareBazaar"
+    ],
+    "SHA1": [
+        "VirusTotal",
+        "MalwareBazaar"
+    ],
+    "SHA256": [
+        "VirusTotal",
+        "MalwareBazaar"
+    ],
+    "EMAIL": [
+        "VirusTotal",
+        "WHOIS"
+    ]
 }
 
 
 def create_investigation_plan(investigation: Investigation) -> Investigation:
-    tools = TOOL_MAP.get(investigation.ioc_type, [])
-
-    investigation.investigation_plan = tools
+    investigation.investigation_plan = TOOL_MAP.get(
+        investigation.ioc_type,
+        []
+    ).copy()
 
     return investigation
